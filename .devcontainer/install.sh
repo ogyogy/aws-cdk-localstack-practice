6c345bfb8fe52c3d319f0fdc5cc8c774157cb44c
ogyogy <47146269+ogyogy@users.noreply.github.com>
2022-12-07 08:03:39 +0000

    diagramsのインストールを追加

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-pip3 install --user awscli-local
+sudo apt update && sudo apt -y install graphviz 
+pip3 install --user awscli-local diagrams
 npm install -g npm install -g aws-cdk-local aws-cdk
